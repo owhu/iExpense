ab4699790a407ae1da0e9a74b2f462bd03ed5e12
oliver <oliverhu.oh@gmail.com>
2024-08-05 09:36:05 -0700

    project updated - accessibility option added

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,53 +8,54 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.modelContext) var modelContext
     @Environment(\.dismiss) var dismiss
     
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
     
-    var expenses: Expenses
-
-    let types = ["Business", "Personal"]
+    //static allows us to read externally
+    static let types = ["Business", "Personal"]
     
     let localCurrency = Locale.current.currency?.identifier ?? "USD"
 
     var body: some View {
-        //        NavigationStack {
-        Form {
-                            TextField("Name", text: $name)
-            
-            Picker("Type", selection: $type) {
-                ForEach(types, id: \.self) {
-                    Text($0)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: localCurrency))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        let item = ExpenseItem(name: name, type: type, amount: amount)
+                        modelContext.insert(item)
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", role: .cancel) {
+                        dismiss()
+                    }
                 }
             }
-            
-            TextField("Amount", value: $amount, format: .currency(code: localCurrency))
-                .keyboardType(.decimalPad)
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle("Add new expense")
-        .toolbar {
-            ToolbarItem(placement: .confirmationAction) {
-                Button("Save") {
-                    let item = ExpenseItem(name: name, type: type, amount: amount)
-                    expenses.items.append(item)
-                    dismiss()
-                }
-            }
-            ToolbarItem(placement: .cancellationAction) {
-                Button("Cancel", role: .cancel) {
-                    dismiss()
-                }
-            }
-        }
-        .navigationBarBackButtonHidden()
-        .navigationBarTitleDisplayMode(.inline)
-        //        }
     }
 }
 
 #Preview {
-    AddView(expenses: Expenses())
+    AddView()
+        .modelContainer(for: ExpenseItem.self)
 }
--- iExpense/ExpensesList.swift
+++ iExpense/ExpensesList.swift
@@ -1,22 +1,22 @@
 //
-//  ExpenseSection.swift
+//  ExpensesList.swift
 //  iExpense
 //
 //  Created by Oliver Hu on 2/7/24.
 //
 
 import SwiftUI
+import SwiftData
 
-struct ExpenseSection: View {
-    let title: String
-    let expenses: [ExpenseItem]
-    let deleteItems: (IndexSet) -> Void
+struct ExpensesList: View {
+    @Environment(\.modelContext) var modelContext
+    @Query private var expenses: [ExpenseItem]
    
     let localCurrency = Locale.current.currency?.identifier ?? "USD"
     
     
     var body: some View {
-        Section(title) {
+        List {
             ForEach(expenses) { item in
                 HStack {
                     VStack(alignment: .leading) {
@@ -30,14 +30,33 @@ struct ExpenseSection: View {
                         .style(for: item)
                     
                 }
+                .accessibilityElement()
+                .accessibilityLabel("\(item.name), \(item.amount.formatted(.currency(code: "USD")))")
+                .accessibilityHint(item.type)
             }
-            .onDelete(perform: deleteItems)
+            .onDelete(perform: removeItems)
+        }
+    }
+    
+    init(type: String = "All", sortOrder: [SortDescriptor<ExpenseItem>]) {
+        _expenses = Query(filter: #Predicate {
+            if type == "All" {
+                return true
+            } else {
+                return $0.type == type
+            }
+        }, sort: sortOrder)
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        for offset in offsets {
+            let item = expenses[offset]
+            modelContext.delete(item)
         }
     }
 }
 
 #Preview {
-    ExpenseSection(title: "Personal", expenses: []) { _ in
-        
-    }
+    ExpensesList(sortOrder: [SortDescriptor(\ExpenseItem.name)])
+        .modelContainer(for: ExpenseItem.self)
 }
